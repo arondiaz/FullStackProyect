d337425710e806f00d84fbad7f0cfde53abeb0b2
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-04 18:36:20 -0300

    add dotenv and config environment variables

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
+connectDB();
 
 const port = 4000;
 
