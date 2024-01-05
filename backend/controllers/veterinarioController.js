4ad87c9d13ea76096cc34607335c6f018739c63a
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-05 19:05:50 -0300

    add validation if  user already exist in database

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -3,6 +3,13 @@ import Veterinario from "../models/Veterinario.js";
 const registrar = async (req, res) => {
   const { nombre, email, contraseña } = req.body;
 
+  //Comprobar que el usuario ya está registrado
+  const existeUsuario = Veterinario.findOne({ email });
+  if (existeUsuario) {
+    const error = new Error("El usuario ya está registrado");
+    return res.status(400).json({msg: error.message});
+  }
+
   try {
     const veterinario = new Veterinario(req.body);
     const nuevoVeterinario = await veterinario.save();
