048d8b51839f2d4d86a9b62b1aabef8b0bfddb03
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-08 14:54:42 -0300

    add bcrypt and hash password

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,4 +1,3 @@
-import { json } from "express";
 import Veterinario from "../models/Veterinario.js";
 
 const registrar = async (req, res) => {
--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import generarId from "../helpers/generarId.js";
+import bcrypt from "bcrypt";
 
 const veterinarioSchema = mongoose.Schema({
   nombre: {
@@ -39,6 +40,19 @@ const veterinarioSchema = mongoose.Schema({
     type: Boolean,
     default: false,
   },
+});
+
+veterinarioSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+
+  // const salt = await bcrypt.genSalt(10)
+  // this.password = await bcrypt.hash(this.password, salt)
 });
 
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
