9d378c9eaa6a5da56459ed82e1d79483d6be28e4
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-15 10:26:06 -0300

    create initial config of middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,6 @@
+const checkAuth = (req, res, next) => {
+  console.log("desde middleware");
+
+  next();
+};
+export default checkAuth;
--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { registrar, perfil, confirmar, login } from "../controllers/veterinarioController.js";
+import checkAuth from "../middleware/authMiddleware.js";
 
 const routes = express.Router();
 
 routes.post("/", registrar)
-
-routes.get("/perfil", perfil)
-
 routes.get("/confirmar/:token", confirmar)
-
 routes.post("/login", login)
+
+routes.get("/perfil", checkAuth, perfil)
 
 export default routes;
