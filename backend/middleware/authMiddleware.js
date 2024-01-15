aa03abf1d21066a930bb9f392c7975c46041ba48
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-15 16:18:27 -0300

    add auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,9 +1,28 @@
-const checkAuth = (req, res, next) => {
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-    console.log("Si tiene token");
+import jwt from "jsonwebtoken";
+import Veterinario from "../models/Veterinario.js";
+
+const checkAuth = async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.veterinario = await Veterinario.findById(decoded.id).select("-password -token -confirmado");
+
+      return next();
+
+    } catch (error) {
+      const e = new Error("Token no válido");
+      res.status(401).json({ msg: e.message });
+    }
   }
-  const error = new Error("Token no válido");
-  res.status(403).json({ msg: error.msg });
+
+  if (!token) {
+    const error = new Error("Token inexistente");
+    res.status(403).json({ msg: error.message });
+  }
 
   next();
 };
