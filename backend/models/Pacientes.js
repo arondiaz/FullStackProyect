03d811d5ea1a924aa7b80f9aa76501938034459d
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-16 16:16:22 -0300

    add client model

---
+++ backend/models/Pacientes.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const pacientesShema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    propietario: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    fecha: {
+      type: Date,
+      required: true,
+    },
+    sintomas: {
+      type: String,
+      required: true,
+    },
+    veterinario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Veterinario",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Paciente = mongoose.model("Paciente", pacientesShema);
+
+export default Paciente;
