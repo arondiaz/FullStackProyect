db1f0100408e25a2c95ec7a2a7eaf913df6f9d88
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-19 16:39:02 -0300

    add login form

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,11 +3,13 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-    <h1>Authlayout</h1>
+      <h1>Authlayout</h1>
 
-    <Outlet/>
+      <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-12">
+        <Outlet />
+      </main>
     </>
-  )
+  );
 };
 
 export default AuthLayout;
--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,5 +1,44 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión</h1>
+      </div>
+
+      <div>
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="text"
+              placeholder="Ingresa tu email"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Ingresa tu password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Iniciar sesión"
+            className="bg-indigo-700 py-2 px-6 rounded-xl w-full text-white font-bold mt-5 cursor-pointer hover:bg-indigo-800
+            md:w-auto"
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
