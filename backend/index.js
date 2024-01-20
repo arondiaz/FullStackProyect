e1d570631d621e6ad36adbb80d54637d479ea0bf
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-20 10:40:22 -0300

    enable cors

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacientesRoutes from "./routes/pacienteRoutes.js";
+import cors from "cors";
 
 const app = express();
 
@@ -12,6 +13,20 @@ app.use(express.json());
 dotenv.config();
 
 connectDB();
+
+const dominiosPermitidos = ["http://localhost:4000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+
+app.use(cors({ origin: '*' }));
 
 const port = 4000;
 
