f3de69d0868db0842ac08448ba47eab196104ce2
Arón Díaz <arondiaz.cpn@gmail.com>
2024-01-21 17:03:07 -0300

    add confirm account

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,55 @@
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Alerta from "../components/Alerta";
+
 const ConfirmarCuenta = () => {
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+  const [cargando, setCargando] = useState(true);
+  const [alerta, setAlerta] = useState();
+
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        const url = `http://localhost:4000/api/veterinarios/confirmar/${id}`;
+        const { data } = await axios.get(url);
+        setCuentaConfirmada(true);
+        setAlerta({
+          msg: data.msg,
+          error: false,
+        });
+      } catch (error) {
+        setAlerta({ msg: error.response.data.msg, error: true });
+      }
+      setCargando(false);
+    };
+
+   return () =>  confirmarCuenta();
+  }, []);
+
   return (
     <>
-      <h2>Confirmar cuenta</h2>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Confirma tu cuenta
+        </h1>
+      </div>
+
+      <div className="mt-20 md:mt-5">
+        {!cargando && <Alerta alerta={alerta} />}
+
+        {cuentaConfirmada && (
+          <Link
+            to="/"
+            className="text-lg block text-center my-5 text-gray-500"
+          >
+            Iniciar Sesión{" "}
+          </Link>
+        )}
+      </div>
     </>
   );
 };
